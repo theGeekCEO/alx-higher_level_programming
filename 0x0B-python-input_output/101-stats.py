6f9e483c4223a4f6af727928ee0f6a64d7efdfeb
theGeekCEO <pixxellagemedia@gmail.com>
2023-01-11 08:55:08 -0800

    Python - Input/Output

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Read from standard input and cal metrics.
+
+After each ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following stats:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
+"""
+
+
+def print_stats(size, status_codes):
+    """Print recorded metrics.
+
+    Args:
+        size (int): The recorded read file size.
+        status_codes (dict): The recorded count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    val_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in val_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
